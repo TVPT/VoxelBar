907a80cb0ab856d20e83033f3f626bdb523ab274
keto23 <kethomassen@gmail.com>
2012-12-12 06:57:55 +1000

    Getting ready for new toggle manager

--- src/main/java/com/thevoxelbox/voxelbar/VoxelBarListener.java
+++ src/main/java/com/thevoxelbox/voxelbar/VoxelBarListener.java
@@ -7,7 +7,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 
 public class VoxelBarListener implements Listener {
-    private VoxelBar vb = new VoxelBar();
     
     @EventHandler
     public void onItemHeldChange(PlayerItemHeldEvent event) {
@@ -16,17 +15,17 @@ public class VoxelBarListener implements Listener {
         if ((p.isSneaking())) {
             if ((difference == 1)) {
                 if (p.isOp() || p.hasPermission("voxelbar.use")) {
-                    if (VoxelBarFunctions.isEnabled(p.getName())) {
+                    //if (VoxelBarFunctions.isEnabled(p.getName())) {
                         VoxelBarFunctions.moveInventory(p, 1);
                         p.sendMessage(ChatColor.GREEN + "You moved your inventory " + ChatColor.RESET + ChatColor.UNDERLINE + ChatColor.DARK_AQUA +"backwards");
-                    }
+                    //}
                 }
             } else if ((difference == -1)) {
                 if (p.isOp() || p.hasPermission("voxelbar.use")) {
-                    if (VoxelBarFunctions.isEnabled(p.getName())) {
+                    //if (VoxelBarFunctions.isEnabled(p.getName())) {
                         VoxelBarFunctions.moveInventory(p, 3);
                         p.sendMessage(ChatColor.GREEN + "You moved your inventory " + ChatColor.RESET + ChatColor.UNDERLINE + ChatColor.GOLD +"forward");   
-                    }
+                    //}
                 }
             }
         }
