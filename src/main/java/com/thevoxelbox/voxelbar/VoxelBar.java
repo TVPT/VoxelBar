2b7313d246e931ad94a7f7683ac21dddb5e991e9
keto23 <kethomassen@gmail.com>
2012-12-12 06:55:57 +1000

    Getting ready for new toggle manager

--- src/main/java/com/thevoxelbox/voxelbar/VoxelBar.java
+++ src/main/java/com/thevoxelbox/voxelbar/VoxelBar.java
@@ -4,26 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class VoxelBar extends JavaPlugin {
 
-    
-    @Override
-    public void onDisable() {  
-        VoxelBarFunctions.savePlayers();
-    }
     @Override
     public void onEnable() {
         
         getServer().getPluginManager().registerEvents(new VoxelBarListener(), this); // Register VoxelBarListener
         
         getCommand("vbar").setExecutor(new VoxelBarCommands()); // Register VoxelBarCommands
-
-        
-
-        saveDefaultConfig();
-        
-        VoxelBarFunctions.loadPlayers();
-        
+ 
     }
-
-    
+  
 }
 
