fdad63aac0d7ce784e38fa83ac432a24aadde2b7
keto23 <kethomassen@gmail.com>
2012-12-12 06:57:26 +1000

    Getting ready for new toggle manager

--- src/main/java/com/thevoxelbox/voxelbar/VoxelBarFunctions.java
+++ src/main/java/com/thevoxelbox/voxelbar/VoxelBarFunctions.java
@@ -6,33 +6,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 public class VoxelBarFunctions {
-    private static VoxelBar vb = new VoxelBar();
-    private static HashMap<String, Boolean> playersEnabled = new HashMap();
-    
-    public static void setStatus(String player, boolean val) {
-        playersEnabled.put(player, val);
-   
-    }
-    public static void savePlayers() {
-        for (String p : playersEnabled.keySet()) {
-            vb.getConfig().set("players." + p, playersEnabled.get(p));
-        }
-        vb.saveConfig();
-    }
-    public static void loadPlayers() {
-        if (vb.getConfig().contains("players")) {
-            for (String p : vb.getConfig().getStringList("players")) {
-                playersEnabled.put(p, vb.getConfig().getBoolean("players." + p, false));
-            }
-        }
-    }
-    
-    public static boolean isEnabled(String player) {
-        if (playersEnabled.containsKey(player)) {
-            return playersEnabled.get(player);
-        }
-        return true;
-    }
     
     public static void moveInventory(Player player, int rows) {
         PlayerInventory inv = player.getInventory();
