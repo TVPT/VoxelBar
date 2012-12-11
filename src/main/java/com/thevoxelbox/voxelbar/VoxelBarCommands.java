956d5549ae28c2ab6b3f0aef190556ff561a06a9
keto23 <kethomassen@gmail.com>
2012-12-11 15:47:59 +1000

    Fixed the ToggleManager

--- src/main/java/com/thevoxelbox/voxelbar/VoxelBarCommands.java
+++ src/main/java/com/thevoxelbox/voxelbar/VoxelBarCommands.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 
 public class VoxelBarCommands implements CommandExecutor {
     private VoxelBar vb = new VoxelBar();
-    private VoxelBarToggleManager tm = vb.tm;
     public boolean onCommand(CommandSender cs, Command cmnd, String label, String[] args) {
         // Check if player is console.
         if (!(cs instanceof Player)) {
@@ -27,21 +26,21 @@ public class VoxelBarCommands implements CommandExecutor {
         // Set command to the first arg of the command
         command = args[0];
         if (command.equalsIgnoreCase("enable") || command.equalsIgnoreCase("on") || command.equalsIgnoreCase("e")) { // Check if they're running the /vbar enable command
-                if (tm.isEnabled(player.getName())) {
+                if (vb.tm.isEnabled(player.getName())) {
                     player.sendMessage(ChatColor.GREEN + "VoxelBar already enabled for you! Type " + ChatColor.DARK_AQUA + "/vbar disable" + ChatColor.GREEN + " to disable VoxelBar!");
                     return true;
                 }
-                tm.setStatus(player.getName(), true);
+                vb.tm.setStatus(player.getName(), true);
                 player.sendMessage(ChatColor.GREEN + "VoxelBar enabled for you - " + ChatColor.DARK_AQUA + "/vbar disable" + ChatColor.GREEN + " to turn it off again!");
                 return true;
                 
         } else if (command.equalsIgnoreCase("disable") || command.equalsIgnoreCase("off") || command.equalsIgnoreCase("d")) { // Check if they're running the /vbar disable command
-                if (tm.isEnabled(player.getName())) {
-                    tm.setStatus(player.getName(), false);
+                if (vb.tm.isEnabled(player.getName())) {
+                    vb.tm.setStatus(player.getName(), false);
                     player.sendMessage(ChatColor.GREEN + "VoxelBar disabled for you - " + ChatColor.DARK_AQUA + "/vbar enable" + ChatColor.GREEN + " to turn it on again!");   
                     return true;
                 }
-                player.sendMessage(ChatColor.GREEN + "VoxelBar already disabled for you! Type " + ChatColor.DARK_AQUA + "/vbar enable" + ChatColor.GREEN + " to enable VoxelBar!");
+                player.sendMessage(ChatColor.GREEN + "VoxelBar already enabled for you! Type " + ChatColor.DARK_AQUA + "/vbar disable" + ChatColor.GREEN + " to disable VoxelBar!");
                 return true;
                     
         
